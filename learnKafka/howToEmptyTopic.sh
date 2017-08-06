83de830f30dea1fc0d9fd9048c8e17e3d20ba3f5
traderres <traderres@yahoo.com>
2017-08-06 17:39:00 -0400

    Corrected it so that it works every time (by waiting for 5 minutes)

--- learnKafka/howToEmptyTopic.sh
+++ learnKafka/howToEmptyTopic.sh
@@ -13,19 +13,23 @@ Procedure
 
     NOTE:  Remove any leading spaces
 
-        #!/bin/bash
+    #!/bin/bash
 
-        echo "Enter name of topic to empty:"
-        read topicName
+    echo "Enter name of topic to empty:"
+    read topicName
 
-        echo Setting the retention.ms to be 1000
-        $KAFKA_HOME/bin/kafka-configs.sh --zookeeper localhost:2181 --alter --entity-type topics --entity-name $topicName --add-config retention.ms=1000
+    echo Setting the retention.ms=1000
+    $KAFKA_HOME/bin/kafka-configs.sh --zookeeper localhost:2181 --alter --entity-type topics --entity-name $topicName --add-config retention.ms=1000
 
-        echo 'sleeping for 90 secs (so kafka will process the change)'
-        sleep 90
+    # Sleep for 300 seconds
+    #   log.retention.check.interval.ms has a default value of 300000
+    #   This setting determines the frequency that the log cleaner checks whether any log is eligible for deletion
+    echo 'sleeping for 5 minutes (so kafka will process the changes)'
+    echo I will return at `date --date 'now + 300 seconds' `
+    sleep 300
 
-        echo Deleting the rention.ms time
-        $KAFKA_HOME/bin/kafka-configs.sh --zookeeper localhost:2181 --alter --entity-type topics --entity-name $topicName --delete-config retention.ms
+    echo Deleting the rention.ms setting
+    $KAFKA_HOME/bin/kafka-configs.sh --zookeeper localhost:2181 --alter --entity-type topics --entity-name $topicName --delete-config retention.ms
 
 
  2. Make this script executable
@@ -60,8 +64,8 @@ Procedure
  7. Run the emptyTopic.sh script
     unix> ./emptyTopic.sh
 
-    Enter name of topic to delete from zookeeper: stuff <enter>
-
+    Enter name of topic to delete from zookeeper:
+    stuff <enter>
 
  8. Use the consumer to pull those messages (from the beginning)
     unix> cd $KAFKA_HOME
