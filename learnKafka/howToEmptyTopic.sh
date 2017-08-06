d94829371a31b2bf82ae194675c28021ea9a680f
traderres <traderres@yahoo.com>
2017-08-06 16:58:14 -0400

    Tested

---
+++ learnKafka/howToEmptyTopic.sh
@@ -0,0 +1,61 @@
+How to Empty a Kafka Topic (fully)
+----------------------------------
+
+Assumptions:
+ A) You have a $KAFKA_HOME variable set
+
+
+Procedure
+---------
+ 1. Create this bash script
+    unix> vi emptyTopic.sh
+
+        #!/bin/bash
+        echo "Enter name of topic to empty:"
+        read topicName
+        $KAFKA_HOME/bin/kafka-configs.sh --zookeeper localhost:2181 --alter --entity-type topics --entity-name $topicName --add-config retention.ms=1000
+        echo sleeping for 90 secs (so kafka will process the change)
+        sleep 90
+        $KAFKA_HOME/bin/kafka-configs.sh --zookeeper localhost:2181 --alter --entity-type topics --entity-name $topicName --delete-config retention.ms
+
+
+ 2. Make this script executable
+    unix> chmod ugo+x emptyTopic.sh
+
+
+ 3. Create a sample topic called "stuff"
+    unix> cd $KAFKA_HOME
+    unix> bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic stuff
+
+
+ 4. List the topics
+    unix> cd $KAFKA_HOME
+    unix>  bin/kafka-topics.sh --list --zookeeper localhost:2181
+    -- You should see the topic called "stuff"
+
+
+ 5. Add some data to the topic
+    unix> cd $KAFKA_HOME
+    unix> bin/kafka-console-producer.sh --broker-list localhost:9092 --topic stuff
+        message #1
+        message #2
+
+
+ 6. Use the consumer to pull those messages (from the beginning)
+    unix> cd $KAFKA_HOME
+    unix> bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic stuff --from-beginning
+
+    NOTE:  You should see message #1 and message #2
+
+
+ 7. Run the emptyTopic.sh script
+    unix> ./emptyTopic.sh
+
+    Enter name of topic to delete from zookeeper: stuff <enter>
+
+
+ 8. Use the consumer to pull those messages (from the beginning)
+    unix> cd $KAFKA_HOME
+    unix> bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic stuff --from-beginning
+
+     -- There should be no messages
