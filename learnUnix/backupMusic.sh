86c43eae83d5cf25e96c64be5dd1f809bd552605
traderres <traderres@yahoo.com>
2022-08-14 22:52:04 -0400

    Added info about rclone config file

--- learnUnix/backupMusic.sh
+++ learnUnix/backupMusic.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#######################################################
+######################################################################################################
 # Filename:  backupMusic.sh
 # Author:    Adam
-#######################################################
+######################################################################################################
 # Purpose:   To backup files to my google drive
 #
 # Usage:  
@@ -13,9 +13,19 @@
 #  2. Watch the log file:
 #     unix> tail -f backupMusic.log
 #
-# NOTE: 
-#   --transfers 50 means to transfer 50 files in parallel
-#######################################################
+# Assumptions:
+#  A. The rclone config file (found in ~/.config/rclone/rclone.conf) holds something like this:
+#      [google-drive]
+#      type = drive
+#      client_id = client_id_is_here
+#      client_secret = client_secret_is_here
+#      scope = drive
+#      token = {"access_token":"really-long-token-is-here","token_type":"Bearer","refresh_token":"1//another-really-long-string-here","expiry":"2022-08-14T23:45:34.520070061-04:00"}
+#      root_folder_id = root_folder_id
+#
+# NOTE:
+#   --transfers 10 means to transfer 10 files in parallel  (if you go too high then google complains)
+######################################################################################################
 
 readonly SCRIPT_NAME=`basename $0`                   # Holds the name of this bash script
 
@@ -24,7 +34,7 @@ function main()
 {
   # Sync the source and destination so that they are *identical*
   # NOTE:  For a list of options, go to https://rclone.org/flags/ 
-  rclone sync --verbose --transfers 50 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s  "/mnt/windows/iTunes" "google-drive:/backup/iTunes"
+  rclone sync --verbose --transfers 10 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s  "/mnt/windows/iTunes" "google-drive:/backup/iTunes"
 
 
   if [ $? -ne 0 ]; then
